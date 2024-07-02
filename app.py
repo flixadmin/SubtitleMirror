b254f29fd81e04154ea96a41365bb0ab876238e4
flixadmin <159233415+flixadmin@users.noreply.github.com>
2024-07-02 11:39:44 +0000

    f

--- app.py
+++ app.py
@@ -1,17 +1,43 @@
-from flask import Flask, request, Response
+from flask import Flask, request, jsonify, url_for, Response
 from requests import get
-from urllib.parse import unquote
+import os, magic
 
 app = Flask(__name__)
+upload_dir = os.path.join(os.getcwd(), 'uploads')
+try: os.mkdir(upload_dir)
+except FileExistsError: pass
+
+def get_mime(file):
+    return magic.from_file(file, True)
 
 @app.route('/')
 def index():
-    print('User-agent:', request.user_agent, flush=True)
-    url = unquote(request.args.get('url', ''))
-    if not url: return 'Usage: /?url={subtitle-url}'
-    print(url)
-    r = get(url)
-    return Response(r.content, r.status_code, content_type=r.headers.get('Content-Type'))
+    return 'Usage: <br>POST /upload/{filename}?key={apikey}&slug={videoslug}&lang={Language} and data={FILE_DATA}'
+
+
+@app.route('/upload/<filename>', methods=['POST'])
+def upload(filename):
+    key = request.args.get('key')
+    slug = request.args.get('slug')
+    lang = request.args.get('lang')
+    try:
+        path = os.path.join(upload_dir, filename)
+        with open(path, 'wb') as f: f.write(request.data)
+        dlurl = 'https://' + request.host + '/download/' + filename
+        print(dlurl)
+        r = get(f'https://api.hydrax.net/{key}/subtitle/{slug}', headers={'Content-Type': 'application/json'}, data='{"url":"' + dlurl + '","label":"' + lang + '"}')
+        # os.remove(path)
+        return jsonify(r.json())
+    except Exception as e:
+        return jsonify(dict(status=False, msg='Error while saving file: ' + str(e)), status=500)
+
+@app.route('/download/<filename>')
+def download(filename):
+    try:
+        with open(os.path.join(upload_dir, filename), 'rb') as f:
+            return Response(f.read(), 200, mimetype=get_mime())
+    except Exception as e:
+        return jsonify(dict(success=False, msg='Error while sending file: ' + str(e)), status=404)
 
 
 if __name__ == '__main__':
